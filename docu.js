0a2ca1f3011a75f3955839beed99b9085b75fdc7
EdoardoSquillaci <edoardosquillaci@gmail.com>
2024-05-15 18:22:07 +0200

    Aggiunta Logiche

--- docu.js
+++ docu.js
@@ -1,3 +1,43 @@
 'use strict'
 
-console.log(document)
+
+
+
+//Form
+const calcolatorePrezzo = document.getElementById('formBiglietto')
+
+//input utente
+
+const etaUtente = document.getElementById ('age');
+const kmUtente = document.getElementById ('km');
+
+//calcoli
+
+const prezzo = 0.21
+constMinorenni = 18
+constOver = 65
+const scontoMinorenni = 20
+const scontoOver = 40
+
+
+//Form Submit aggiungo l'evento click
+
+
+document.getElementById('formBiglietto').addEventListener('submit' , function(event){
+
+event.preventDefault();
+
+const etaUtenteValue = Number (etaUtente.value);
+const kmUtenteValue = Number (kmUtente.value)
+
+
+//logica di calcolo
+
+
+ 
+
+
+
+
+
+})
